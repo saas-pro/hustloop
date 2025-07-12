3487c2e87aac0c121dcf5061963ef869c7e3c397
Boopathi <7frcw6ye7@relay.firefox.com>
2025-07-12 05:47:10 +0530

    Update page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import { Suspense } from "react";
 import MainView from "@/components/views/main-view";
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
-  return <MainView />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <MainView />
+    </Suspense>
+  );
 }
