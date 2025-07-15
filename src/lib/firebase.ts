b0c77bebd4eabac2100eb79e71447f3c79d483e5
Boopathi <7frcw6ye7@relay.firefox.com>
2025-07-16 04:18:25 +0530

    Verified build and pushed latest update

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@
 import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDEv3CHNo3X0pg0ssG8hdO1-mJNPpXlE-s",
-  authDomain: "hustloop.firebaseapp.com",
-  projectId: "hustloop",
-  storageBucket: "hustloop.firebasestorage.app",
-  messagingSenderId: "1059618765323",
-  appId: "1:1059618765323:web:443c3e836dc18dd5dba50e",
-  measurementId: "G-G1YRLQ771T"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
 
 // A function to initialize Firebase App, ensuring it's a singleton.
